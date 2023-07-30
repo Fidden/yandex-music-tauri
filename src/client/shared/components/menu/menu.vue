9bdba945a65dd9abec33c89ef29d8f86c2ab5405
storm <sahalisa17@gmail.com>
2023-07-30 22:00:45 +0300

    feat: Add menu component.

---
+++ src/client/shared/components/menu/menu.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import {cnMenu} from './menu.const';
+</script>
+
+<template>
+	<HLMenu
+		as="div"
+		:class="cnMenu()"
+	>
+		<slot/>
+	</HLMenu>
+</template>
+
+<style lang="scss">
+.menu {
+	display: flex;
+	flex-direction: column;
+	position: relative;
+}
+</style>
---
+++ src/client/shared/components/menu/menu__button.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import {cnMenu} from './menu.const';
+</script>
+
+<template>
+	<HLMenuButton
+		as="button"
+		:class="cnMenu('button')"
+	>
+		<slot/>
+	</HLMenuButton>
+</template>
+
+<style lang="scss">
+.menu__button {
+	display: flex;
+	flex-direction: row;
+	background: none;
+}
+</style>
