b6803063146e92d5f11cfa0c52a105719a599253
Aleksandr Lisenkov <aleksandr.lisenkov@arora.pro>
2023-08-07 17:59:53 +0300

    refactor: Rename emitter.

--- src/client/shared/emitters/global.emitter.ts
+++ src/client/shared/emitters/global.emitter.ts
@@ -1,6 +1,8 @@
 import mitt from 'mitt';
 
 export type GlobalEvents = {
-	'tracks-table:play-shuffle': void
+	'tracks-table:play-shuffle': void,
+	'toast:add': string
 }
+
 export const globalEmitter = mitt<GlobalEvents>();
