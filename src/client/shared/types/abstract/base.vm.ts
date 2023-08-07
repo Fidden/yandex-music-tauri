db7132f80dc6c8821fdb3701d6414fdd9e4683a5
Aleksandr Lisenkov <aleksandr.lisenkov@arora.pro>
2023-08-07 18:01:41 +0300

    refactor: Remove globalEmitter getter.

--- src/client/shared/types/abstract/base.vm.ts
+++ src/client/shared/types/abstract/base.vm.ts
@@ -1,14 +1,8 @@
-import {globalEmitter} from '~/client/shared/emitters/global-emitter';
 import {useUserStore} from '~/client/shared/stores/user.store';
 
 export abstract class BaseVm {
-
 	/* Stores */
 	protected get userStore() {
 		return useUserStore();
-	}
-
-	protected get globalEmitter() {
-		return globalEmitter;
 	}
 }
